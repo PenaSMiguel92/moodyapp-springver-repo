f374e80dd94a8ce10c08623b292eb0085030acfe
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-23 01:01:56 -0700

    init

---
+++ src/main/java/com/moodyapp/MoodyappApplication.java
@@ -0,0 +1,13 @@
+package com.moodyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodyappApplication.class, args);
+	}
+
+}
