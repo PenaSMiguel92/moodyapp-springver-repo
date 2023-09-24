4f36f53c2c23fdf540e404862dc16d83503041da
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-23 20:01:55 -0700

    service imp

--- src/main/java/com/moodyapp/controller/Controller.java
+++ src/main/java/com/moodyapp/controller/Controller.java
@@ -1,13 +1,26 @@
 package com.moodyapp.controller;
 
+import com.moodyapp.entity.Account;
+import com.moodyapp.service.AccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controller {
+    AccountService accountService;
+    @Autowired
+    public Controller(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+
     @GetMapping("/accounts")
-    public void getExistingAccounts() {
-        
+    public List<Account> getExistingAccounts() {
+        return this.accountService.getAllAccounts();
     }
 
 }
--- src/main/java/com/moodyapp/service/AccountService.java
+++ src/main/java/com/moodyapp/service/AccountService.java
@@ -1,8 +1,24 @@
 package com.moodyapp.service;
+import com.moodyapp.entity.Account;
+import com.moodyapp.repository.AccountDAI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountService {
+    AccountDAI accountDAI;
+    @Autowired
+    public AccountService(AccountDAI accountDAI) {
+        this.accountDAI = accountDAI;
+    }
+
+    public List<Account> getAllAccounts() {
+        return this.accountDAI.findAll();
+    }
+
     
 }
