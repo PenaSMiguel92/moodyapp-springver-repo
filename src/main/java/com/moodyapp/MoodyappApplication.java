fdf565c0345d8997612bda42538d3db3d988e00f
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-10-01 17:00:00 -0700

    config modified

--- src/main/java/com/moodyapp/MoodyappApplication.java
+++ src/main/java/com/moodyapp/MoodyappApplication.java
@@ -2,8 +2,10 @@ package com.moodyapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yml")
 public class MoodyappApplication {
 
 	public static void main(String[] args) {
